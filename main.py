f4a33ed291b935bf9b3823b782e205b7ca15f464
YitavGil <yitavga@yahoo.com>
2023-03-01 08:25:50 +0200

    create new posts

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [
+    {
+    "title": "title of post 1",
+    "content": "content of post 1",
+    "id": 1
+},
+  {
+    "title": "best food",
+    "content": "pizza",
+    "id": 2
+},
+]
 
 @app.get("/")
 def root():
@@ -9,9 +31,11 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createpost")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title {payload['title']} conetnt: {payload['content']}"}
+@app.post("/posts")
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 999999)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
