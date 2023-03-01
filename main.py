5cacbbd542361236c85b13da935214d8bd586d5c
YitavGil <yitavga@yahoo.com>
2023-03-01 07:09:25 +0200

    post requests

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "welcome to my server"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+@app.post("/createpost")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title {payload['title']} conetnt: {payload['content']}"}
